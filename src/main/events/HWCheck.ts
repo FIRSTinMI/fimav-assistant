2c04673feb511a70be90584cabab6ddf62ffd369
Soren Zaiser <soren.zaiser@gmail.com>
2023-03-22 12:36:32 -0400

    More things

--- src/main/events/HWCheck.ts
+++ src/main/events/HWCheck.ts
@@ -4,15 +4,30 @@ import { networkInterfaces, hostname } from "os";
 
 
 export default function HWCheck(): HWCheckResponse {
+    // Get Computer Name
     const whoAmI = hostname();
+    // AV Carts are maned FIMAV<number>, so lets determine which cart we are
+    let cartNumber = parseInt(whoAmI.replace("FIMAV", ""));
+    // If NaN, we are not an AV Cart
+    const isAVCart = !isNaN(cartNumber);
+    // Set cart number to 1 is NaN
+    if (!isAVCart) cartNumber = 1;
+    // Get Network Interfaces
     const interfaces = networkInterfaces();
-    const interfaceNames = Object.keys(interfaces).filter(i => i.toLowerCase().indexOf("vlan") > -1);
-    // TODO: This is broke
-    const ipReady = Object.values(interfaces).filter(i => Array.isArray(i) && i);
+    // Find important interfaces
+    const vlan10 = interfaces["Vlan10 - Internet"];
+    const vlan20 = interfaces["Vlan20 - Field"];
+    const vlan30 = interfaces["Vlan30 - AV"];
+    // Check that AV Vlan has a static IP of 192.168.25.<cart_number>0
+    const avIp = vlan30?.find((i) => i.family === "IPv4")?.address;
+    const avStatic = avIp === `192.168.25.${cartNumber}0`;
+    
+
 
     return {
         ip_ready: true,
         audio_ready: true,
         network_ready: interfaceNames.length > 0,
+        not_official_hw: !isAVCart
     };
 }
--- src/models/HWCheckResponse.ts
+++ src/models/HWCheckResponse.ts
@@ -2,4 +2,8 @@ export default interface HWCheckResponse {
     ip_ready: boolean;
     audio_ready: boolean;
     network_ready: boolean;
+    venue_static_ip?: string;
+    av_static_ip?: string;
+    unofficial_hw: boolean;
+
 }
